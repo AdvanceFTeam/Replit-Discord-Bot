ba87e72f10a1a40a2e05fdd18cc3c117b1353d7f
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-05-29 03:04:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   prefix: '!', // Change this to your desired command prefix
-  token: process.env.DISCORD_TOKEN, // Make sure you have DISCORD_TOKEN defined in your .env file
-  API_BASE_URL: 'https://api-popcord.vercel.app', // Update this URL if necessary
+  token: process.env.DISCORD_TOKEN, // Make sure you have DISCORD_TOKEN defined in your Secret
+  API_BASE_URL: 'https://api-popcord.vercel.app', // the URL for the sfw and nsfw
 };
