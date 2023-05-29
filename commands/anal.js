624b876959ed4f5d5457890d0c279b1c72fde4e0
Steven <116656099+StevenK-293@users.noreply.github.com>
2023-05-29 02:30:11 -0400

    Update anal.js

--- commands/anal.js
+++ commands/anal.js
@@ -3,29 +3,29 @@ const { MessageEmbed } = require('discord.js');
 const { API_BASE_URL } = require('../config');
 
 module.exports = {
-  name: 'anal',
-  description: '**(NSFW)** Returns a randomly selected anal content.',
-  usage: '**!anal**',
+  name: 'boobs',
+  description: '**(NSFW)** Returns a randomly selected boobs content.',
+  usage: '**!boobs**',
   execute(message) {
     // Check if the command is used in an NSFW channel
     if (!message.channel.nsfw) {
       return message.reply('This command can only be used in NSFW channels.');
     }
 
-    // Make a GET request to the API to fetch NSFW content of type "anal"
-    axios.get(`${API_BASE_URL}/img/nsfw?type=anal`)
+    // Make a GET request to the API to fetch NSFW content
+    axios.get(`${API_BASE_URL}/img/nsfw?type=boobs`)
       .then(response => {
         const nsfwContent = response.data;
 
-        // Create a new MessageEmbed to display the NSFW content
+        // Create a message embed to send the NSFW content
         const embed = new MessageEmbed()
-          .setTitle('NSFW Content - Anal')
+          .setTitle('NSFW Content - Boobs')
           .setDescription('Note: Certain images or gifs may not be displayed due to possible deletion or slow loading times.')
           .setImage(nsfwContent.url)
           .setURL(nsfwContent.url) // Set the URL to the original image for direct access
           .setColor('#FF0000');
 
-        // Send the embed to the channel where the command was used
+        // Send the message embed containing the NSFW content
         message.channel.send({ embeds: [embed] });
       })
       .catch(error => {
