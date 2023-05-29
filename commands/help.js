7ff6dd258ae3ac6653e0ee5354e2010c753b9c53
YellowGreg <sksor4@gmail.com>
2023-05-29 02:33:09 -0400

    Update help.js
    Added Comment

--- commands/help.js
+++ commands/help.js
@@ -15,6 +15,7 @@ module.exports = {
       const endIndex = startIndex + pageSize;
       const visibleCommands = commandArray.slice(startIndex, endIndex);
 
+      // Generate a formatted string for each command with its name, description, and usage
       const commandDescriptions = visibleCommands.map((command) => {
         const description = command.description || 'No description provided.';
         const usage = command.usage || 'No usage information provided.';
@@ -27,12 +28,14 @@ module.exports = {
     const updateHelpMessage = async (helpMessage) => {
       const commandList = generateCommandList();
 
+      // Create a message embed to display the command list
       const embed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Command List')
         .setDescription(`Here is a list of available commands and their usage:\n\n${commandList}`)
         .setFooter(`Page ${currentPage}/${totalPages}`);
 
+      // Create button components for pagination
       const buttonRow = new MessageActionRow()
         .addComponents(
           new MessageButton()
@@ -49,9 +52,11 @@ module.exports = {
             .setDisabled(currentPage === totalPages)
         );
 
+      // Update the help message with the new embed and button row
       await helpMessage.edit({ embeds: [embed], components: [buttonRow] });
     };
 
+    // Initial setup: show loading and prepare the command list message
     message.channel.send('Loading commands...').then(async (loadingMessage) => {
       await loadingMessage.delete();
 
@@ -59,6 +64,7 @@ module.exports = {
 
       updateHelpMessage(helpMessage);
 
+      // Create a collector to handle button interactions
       const filter = (interaction) => {
         return ['previous', 'next'].includes(interaction.customId) && interaction.user.id === message.author.id;
       };
@@ -66,6 +72,7 @@ module.exports = {
       const collector = helpMessage.createMessageComponentCollector({ filter, time: 60000 });
 
       collector.on('collect', async (interaction) => {
+        // Handle previous and next button clicks to navigate through pages
         if (interaction.customId === 'previous' && currentPage > 1) {
           currentPage--;
         } else if (interaction.customId === 'next' && currentPage < totalPages) {
@@ -77,6 +84,7 @@ module.exports = {
       });
 
       collector.on('end', () => {
+        // Disable the previous and next buttons after the collector ends
         const disableButtonRow = new MessageActionRow()
           .addComponents(
             new MessageButton()
@@ -95,6 +103,7 @@ module.exports = {
         helpMessage.edit({ components: [disableButtonRow] }).catch(console.error);
       });
 
+      // Create the previous and next buttons with initial state
       const previousButton = new MessageButton()
         .setCustomId('previous')
         .setLabel('Previous')
@@ -109,8 +118,10 @@ module.exports = {
         .setEmoji('➡️')
         .setDisabled(currentPage === totalPages);
 
+      // Create the button row with the previous and next buttons
       const buttonRow = new MessageActionRow().addComponents(previousButton, nextButton);
 
+      // Update the help message with the button row
       helpMessage.edit({ components: [buttonRow] }).catch(console.error);
     });
   },
