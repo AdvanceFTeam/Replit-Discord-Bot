a070461dcf29a2dba707884c58981a54f4bba055
Wspboy12 <117426385+Wspboy12@users.noreply.github.com>
2023-05-29 02:55:40 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,10 +3,16 @@ module.exports = {
   description: "Checks the bot's latency and API response time.",
   usage: '**!ping**',
   execute(message) {
+    // Record the start time before sending the message
     const startTime = Date.now();
+
+    // Send a message to indicate that the bot is being pinged
     message.channel.send('Pinging...')
       .then((sentMessage) => {
+        // Calculate the end time after the message is sent
         const endTime = Date.now();
+
+        // Create an embed object to display the ping information
         const pingEmbed = {
           color: '#0099ff',
           title: 'Pong! 🏓',
@@ -18,6 +24,7 @@ module.exports = {
           },
         };
 
+        // Edit the original message with the ping information embedded
         sentMessage.edit({ content: '\u200B', embeds: [pingEmbed] });
       })
       .catch(console.error);
