176d1437131b6bc74d48c372df9c39b32eaf40a8
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-05-29 03:07:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 client.commands = new Map();
 
+// Read all command files from the 'commands' directory and add them to the client's commands collection
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -15,22 +16,30 @@ for (const file of commandFiles) {
 const { token, prefix } = require('./config');
 const keepAlive = require('./keep_alive');
 
-client.on('ready', () => {
+// Event handler for when the bot is ready
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
+
+  // Set the bot's status
+  client.user.setActivity('!help', { type: 'LISTENING' });
 });
 
+// Event handler for incoming messages
 client.on('messageCreate', message => {
+  // Ignore messages that don't start with the command prefix or are sent by bots
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
+  // Check if the command exists in the client's commands collection
   if (!client.commands.has(commandName)) return;
 
   const command = client.commands.get(commandName);
 
   try {
-    command.execute(message, args, client.commands); // Pass client.commands as the third argument
+    // Execute the command and pass client.commands as the third argument
+    command.execute(message, args, client.commands);
   } catch (error) {
     console.error(error);
     message.reply('There was an error while executing the command.');
@@ -39,4 +48,8 @@ client.on('messageCreate', message => {
 
 keepAlive(); // Call the keepAlive function to start the server
 
-client.login(token);
+// Log in the bot using the provided token
+client.login(token)
+  .catch(error => {
+    console.error('Failed to log in the bot:', error);
+  });
