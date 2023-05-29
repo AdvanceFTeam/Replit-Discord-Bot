00e9469a6a8578fc1ca6041693325752bf4c1471
YellowGreg <sksor4@gmail.com>
2023-05-29 02:39:16 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,22 +5,27 @@ module.exports = {
   description: 'Kicks a member from the server.',
   usage: '**!kick @member**',
   execute(message, args) {
+    // Check if the user executing the command has the necessary permissions (KICK_MEMBERS)
     if (!message.member.permissions.has('KICK_MEMBERS')) {
       return message.reply('You do not have permission to use this command.');
     }
 
+    // Get the mentioned member from the message
     const member = message.mentions.members.first();
     if (!member) {
       return message.reply('Please mention a member to kick.');
     }
 
+    // Check if the mentioned member can be kicked
     if (!member.kickable) {
       return message.reply('Unable to kick the mentioned member.');
     }
 
+    // Kick the member
     member
       .kick()
       .then(() => {
+        // Create and send an embed message to notify about the kicked member
         const kickEmbed = new MessageEmbed()
           .setColor('#FFA500')
           .setTitle('Member Kicked')
