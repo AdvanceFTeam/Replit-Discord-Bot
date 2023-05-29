6c3db0aa34ca2f02eb448979f0080635ad3be531
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-05-29 02:51:38 -0400

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -5,22 +5,29 @@ module.exports = {
   description: 'Mutes a member in the server.',
   usage: '**!mute @member**',
   execute(message, args) {
+    // Check if the user executing the command has the MUTE_MEMBERS permission
     if (!message.member.permissions.has('MUTE_MEMBERS')) {
       return message.reply('You do not have permission to use this command.');
     }
 
+    // Get the first mentioned member in the message
     const member = message.mentions.members.first();
     if (!member) {
+      // If no member is mentioned, ask the user to mention a member
       return message.reply('Please mention a member to mute.');
     }
 
+    // Find the 'Muted' role in the guild's role cache
     const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
     if (!muteRole) {
+      // If the 'Muted' role doesn't exist, ask the user to create it
       return message.reply('The "Muted" role does not exist. Please create it.');
     }
 
+    // Add the 'Muted' role to the mentioned member
     member.roles.add(muteRole)
       .then(() => {
+        // Create an embed message to notify that the member has been muted
         const muteEmbed = new MessageEmbed()
           .setColor('#FF0000')
           .setTitle('Member Muted')
