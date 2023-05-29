b6c38ca2c9459cf56532743f6f31e83e09ba3d65
Steven <116656099+StevenK-293@users.noreply.github.com>
2023-05-29 02:19:33 -0400

    Update ahegao.js

--- commands/ahegao.js
+++ commands/ahegao.js
@@ -7,19 +7,25 @@ module.exports = {
   description: '**(NSFW)** Returns a randomly selected ahegao content.',
   usage: '**!ahegao**',
   execute(message) {
+    // Check if the command is used in an NSFW channel
     if (!message.channel.nsfw) {
       return message.reply('This command can only be used in NSFW channels.');
     }
 
+    // Make a GET request to the API to fetch NSFW content of type "ahegao"
     axios.get(`${API_BASE_URL}/img/nsfw?type=ahegao`)
       .then(response => {
         const nsfwContent = response.data;
+
+        // Create a new MessageEmbed to display the NSFW content
         const embed = new MessageEmbed()
           .setTitle('NSFW Content - Ahegao')
           .setDescription('Note: Certain images or gifs may not be displayed due to possible deletion or slow loading times.')
           .setImage(nsfwContent.url)
           .setURL(nsfwContent.url) // Set the URL to the original image for direct access
           .setColor('#FF0000');
+
+        // Send the embed to the channel where the command was used
         message.channel.send({ embeds: [embed] });
       })
       .catch(error => {
