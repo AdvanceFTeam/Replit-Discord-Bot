8fbe34d22be4d31119b54f46243b0528a7f2d7d1
Steven <116656099+StevenK-293@users.noreply.github.com>
2023-05-29 02:43:27 -0400

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -7,7 +7,10 @@ module.exports = {
   usage: '**!kill**',
   async execute(message) {
     try {
+      // Send a GET request to the API to fetch a randomly selected kill content
       const response = await axios.get(`${API_BASE_URL}/img/sfw?type=kill`);
+      
+      // Send the URL of the kill content to the channel
       message.channel.send(response.data.url);
     } catch (error) {
       console.error(error);
