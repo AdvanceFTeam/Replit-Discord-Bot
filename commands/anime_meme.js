06145352c5353a5f00de4cbe7cafe8bd311b6491
YellowGreg <sksor4@gmail.com>
2023-05-29 02:32:19 -0400

    Update anime_meme.js

--- commands/anime_meme.js
+++ commands/anime_meme.js
@@ -7,7 +7,10 @@ module.exports = {
   usage: '**!anime_meme**',
   async execute(message) {
     try {
+      // Make a GET request to the API to fetch a randomly selected anime meme (safe for work)
       const response = await axios.get(`${API_BASE_URL}/img/sfw?type=anime_meme`);
+
+      // Send the URL of the anime meme as a message to the channel
       message.channel.send(response.data.url);
     } catch (error) {
       console.error(error);
