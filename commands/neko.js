81bd34d7eba4c2d5b846d127b18183c0d6e5184a
Wspboy12 <117426385+Wspboy12@users.noreply.github.com>
2023-05-29 02:54:53 -0400

    Update neko.js

--- commands/neko.js
+++ commands/neko.js
@@ -7,7 +7,10 @@ module.exports = {
   usage: '**!neko**',
   async execute(message) {
     try {
+      // Send a GET request to the API to fetch a random neko content
       const response = await axios.get(`${API_BASE_URL}/img/sfw?type=neko`);
+
+      // Send the neko content URL to the channel
       message.channel.send(response.data.url);
     } catch (error) {
       console.error(error);
