6b600a78d491d1af3f6d05feba52c0644f6141bc
Wspboy12 <117426385+Wspboy12@users.noreply.github.com>
2023-05-29 02:29:10 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,22 +5,27 @@ module.exports = {
   description: 'Bans a member from the server.',
   usage: '**!ban @member**',
   execute(message, args) {
+    // Check if the user has the necessary permissions to use the command
     if (!message.member.permissions.has('BAN_MEMBERS')) {
       return message.reply('You do not have permission to use this command.');
     }
 
+    // Mentioned member to be banned
     const member = message.mentions.members.first();
     if (!member) {
       return message.reply('Please mention a member to ban.');
     }
 
+    // Check if the mentioned member is bannable
     if (!member.bannable) {
       return message.reply('Unable to ban the mentioned member.');
     }
 
+    // Ban the member
     member
       .ban()
       .then(() => {
+        // Create a ban embed message to send as a reply
         const banEmbed = new MessageEmbed()
           .setColor('#FF0000')
           .setTitle('Member Banned')
@@ -28,6 +33,7 @@ module.exports = {
           .setFooter('AdvanceBot', message.client.user.avatarURL())
           .setTimestamp();
 
+        // Send the ban embed message as a reply
         message.reply({ embeds: [banEmbed] });
       })
       .catch(error => {
